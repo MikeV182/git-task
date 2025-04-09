1b388e2505bd6469eab3791a51da58700d47a373
MikeV182 <michailvays@gmail.com>
2025-04-09 16:09:03 +0700

    completed subtask №2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-# Ожидание запуска контейнера app
-sleep 5
+sleep 10
 
-# docker logs $(docker ps -q --filter "name=app") > /usr/share/nginx/html/index.html
-docker logs app > /usr/share/nginx/html/index.html
+if [ -f /app/test_results/test_results.xml ]; then
+  echo "<html><body><h1>Test Results</h1>" > /usr/share/nginx/html/index.html
+  cat /app/test_results/test_results.xml >> /usr/share/nginx/html/index.html
+  echo "</body></html>" >> /usr/share/nginx/html/index.html
+else
+  echo "<html><body><h1>Error: No test results found!</h1></body></html>" > /usr/share/nginx/html/index.html
+fi
 
 nginx -g 'daemon off;'
+
+
+
+
+
+
+
+
+# sleep 5
+# docker logs app > /usr/share/nginx/html/index.html
+# nginx -g 'daemon off;'
