fdf0ef280fac1c86ca8e2e148b743c5f1552c92e
MikeV182 <michailvays@gmail.com>
2025-04-17 13:25:15 +0700

    added GitLab deployment files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,17 @@
 
 sleep 10
 
+mkdir -p /app/public/
+
 if [ -f /app/test_results/test_results.xml ]; then
-  echo "<html><body><h1>Test Results</h1>" > /usr/share/nginx/html/index.html
-  cat /app/test_results/test_results.xml >> /usr/share/nginx/html/index.html
-  echo "</body></html>" >> /usr/share/nginx/html/index.html
+  echo "<html><body><h1>Test Results</h1>" > /app/public/index.html
+  cat /app/test_results/test_results.xml >> /app/public/index.html
+  echo "</body></html>" >> /app/public/index.html
 else
-  echo "<html><body><h1>Error: No test results found!</h1></body></html>" > /usr/share/nginx/html/index.html
+  echo "<html><body><h1>Error: No test results found!</h1></body></html>" > /app/public/index.html
 fi
 
+# Копируем в стандартный путь Nginx для отображения
+cp -r /app/public/* /usr/share/nginx/html/
+
 nginx -g 'daemon off;'
-
-
-
-
-
-
-
-
-# sleep 5
-# docker logs app > /usr/share/nginx/html/index.html
-# nginx -g 'daemon off;'
